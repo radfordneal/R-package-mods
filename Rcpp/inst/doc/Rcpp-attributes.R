fe3fdf6b9308707deb607fa755d41bffa7bd7787
Radford Neal <radfordneal@gmail.com>
2018-09-02 14:49:44 -0400

    disabled some vignettes in Rcpp

--- Rcpp/inst/doc/Rcpp-attributes.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'Rcpp-attributes.Rnw'
-
-###################################################
-### code chunk number 1: Rcpp-attributes.Rnw:30-32
-###################################################
-prettyVersion <- packageDescription("Rcpp")$Version
-prettyDate <- format(Sys.Date(), "%B %e, %Y")
-
-
