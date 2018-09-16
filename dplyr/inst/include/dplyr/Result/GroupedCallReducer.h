eaaef12a275bc284db6d051d2ced67e396ef7b44
Radford Neal <radfordneal@gmail.com>
2018-09-16 16:57:48 -0400

    added dplyr_0.1.1

---
+++ dplyr/inst/include/dplyr/Result/GroupedCallReducer.h
@@ -0,0 +1,25 @@
+#ifndef dplyr_GroupedCalledReducer_H
+#define dplyr_GroupedCalledReducer_H
+
+namespace dplyr {
+       
+    class GroupedCalledReducer : public CallbackProcessor<GroupedCalledReducer> {
+    public:
+        GroupedCalledReducer(Rcpp::Call call, const LazyGroupedSubsets& subsets, const Environment& env): 
+            proxy(call, subsets, env) 
+        {
+        }
+        
+        virtual ~GroupedCalledReducer(){} ;
+        
+        inline SEXP process_chunk( const SlicingIndex& indices ){
+            return proxy.get(indices) ;
+        }               
+        
+    private:
+        GroupedCallProxy proxy ;
+    } ;
+
+} // namespace dplyr
+
+#endif
